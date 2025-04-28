80ec20f539a23f4d0fe30efe386770d1f4460c32
Sujeong Park <80205505+qkrtnwjd4212@users.noreply.github.com>
2025-04-28 12:29:37 +0900

    Title: 홀수 짝수에 따른 출력값, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 250428/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.py
+++ 250428/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.py
@@ -4,9 +4,6 @@ def f(n):
     if n == 1 or n == 2:
         return n 
     
-    if n % 2 == 0:
-        return f(n-2) + n 
-    else:
-        return f(n-2) + n 
+    return f(n-2) + n 
     
 print(f(N))
