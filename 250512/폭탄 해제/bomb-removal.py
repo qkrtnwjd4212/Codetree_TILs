f6d26d37e12c44cf8631afbb401bae5f950bd897
Sujeong Park <80205505+qkrtnwjd4212@users.noreply.github.com>
2025-05-12 01:22:14 +0900

    Title: 폭탄 해제, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250512/폭탄 해제/bomb-removal.py
@@ -0,0 +1,13 @@
+unlock_code, wire_color, seconds = input().split()
+seconds = int(seconds)
+
+class bomb:
+    def __init__(self, code, color, sec):
+        self.code = code
+        self.color = color
+        self.sec = sec
+
+bomb1 = bomb(unlock_code, wire_color, seconds)
+print(f"code : {bomb1.code}")
+print(f"color : {bomb1.color}")
+print(f"second : {bomb1.sec}")
