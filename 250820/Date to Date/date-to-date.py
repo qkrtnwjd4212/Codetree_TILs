869c54d4ddddfff3f3777e20e75a3f7fca1e5d2d
Sujeong Park <80205505+qkrtnwjd4212@users.noreply.github.com>
2025-08-20 12:58:47 +0900

    Title: Date to Date, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250820/Date to Date/date-to-date.py
+++ 250820/Date to Date/date-to-date.py
@@ -1,9 +1,7 @@
 m1, d1, m2, d2 = map(int, input().split())
 
-num_of_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-if m1 == m2 and d1 == d2:
-    print(1)
-else:
-    print((sum(num_of_days[:m2]) + d2) - (sum(num_of_days[:m1]) + d1))
+
+print((sum(num_of_days[:m2]) + d2) - (sum(num_of_days[:m1]) + d1) + 1)
 
