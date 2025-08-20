621541eb2663a81e99a4d07279d055eca85c4673
Sujeong Park <80205505+qkrtnwjd4212@users.noreply.github.com>
2025-08-20 13:29:39 +0900

    Title: DateTime to DateTime, Time: 41ms, Memory: 16MB, Status: Passed - Codetree

--- 250820/DateTime to DateTime/datetime-to-datetime.py
+++ 250820/DateTime to DateTime/datetime-to-datetime.py
@@ -1,8 +1,8 @@
 a, b, c = map(int, input().split())
 
-if not (a >= 11 and b >= 11 and c >= 11):
+prev = (11 * 24 * 60) + (11 * 60) + 11
+now = (a * 24 * 60) + (b * 60) + c
+if now - prev < 0:
     print(-1)
 else:
-    prev = (11 * 24 * 60) + (11 * 60) + 11
-    now = (a * 24 * 60) + (b * 60) + c
     print(now - prev)
