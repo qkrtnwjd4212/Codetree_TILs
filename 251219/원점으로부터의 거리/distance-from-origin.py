0acb7470602fb97af4d7149a120f0547c4055da9
Sujeong Park <80205505+qkrtnwjd4212@users.noreply.github.com>
2025-12-19 12:12:54 +0900

    Title: 원점으로부터의 거리, Time: 66ms, Memory: 16MB, Status: Passed - Codetree

--- 251219/원점으로부터의 거리/distance-from-origin.py
+++ 251219/원점으로부터의 거리/distance-from-origin.py
@@ -1,19 +1,22 @@
 n = int(input())
 
-points = [(int(i+1), tuple(map(int, input().split()))) for i in range(n)]
-points.sort(key=lambda x: (abs(x[1][0]) + abs(x[1][1])))
-for i in range(n):
-    print(points[i][0])
-
-# class point:
-#     def __init__(self, idx, x, y):
-#         self.idx = idx 
-#         self.x = x
-#         self.y = y
-
-# points = []
+# points = [(int(i+1), tuple(map(int, input().split()))) for i in range(n)]
+# points.sort(key=lambda x: (abs(x[1][0]) + abs(x[1][1])))
 # for i in range(n):
-#     x_, y_ = input().split()
-#     points.append(point(i+1, x_, y_))
+#     print(points[i][0])
 
+class point:
+    def __init__(self, idx, x, y):
+        self.idx = int(idx)
+        self.x = int(x)
+        self.y = int(y)
+
+points = []
+for i in range(n):
+    x_, y_ = input().split()
+    points.append(point(i+1, x_, y_))
+
+points.sort(key=lambda x: (abs(x.x) + abs(x.y)))
+for i in range(n):
+    print(points[i].idx)
 
