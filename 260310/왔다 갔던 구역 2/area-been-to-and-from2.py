c4241bd1c78d36f9f254e13c1a16a0036300abe0
Sujeong Park <80205505+qkrtnwjd4212@users.noreply.github.com>
2026-03-10 16:58:43 +0900

    Title: 왔다 갔던 구역 2, Time: 51ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260310/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 260310/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -7,7 +7,7 @@ for _ in range(n):
     x.append(int(xi))
     dir.append(di)
 
-arr = [0] * (n * 10 * 2 + 1)
+arr = [0] * 2001
 cur = 0
 cnt = 0
 
@@ -15,11 +15,11 @@ for i in range(n):
     if dir[i] == 'R':
         for j in range(cur + 10, cur + x[i] + 10):
             arr[j] += 1
-        cur += x[i]
+        cur += (x[i])
     else:
         for j in range(cur + 10, cur - x[i] + 10, -1):
             arr[j] += 1
-        cur -= x[i]
+        cur -= (x[i])
 
 for x in arr:
     if x >= 2:
